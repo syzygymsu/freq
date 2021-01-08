f5a40b5186a305e34421a632e91d1ce35430434e
Anton Sumin <syzygymsu@gmail.com>
2021-01-08 05:21:12 +0300

    Optimization: read input in big chunks

--- src/parse.h
+++ src/parse.h
@@ -6,21 +6,27 @@
 
 template<typename Callback>
 void Parse(std::istream& input, Callback&& callback) {
-  std::string buffer;
-  char c;
-  while (input.get(c).good()) {
-    c = NormalizeChar(c);  // TODO: use lookup table to normalize chars
-    if (c == normalized::kSpace) {
-      if (!buffer.empty()) {
-        callback(buffer);
-        buffer.clear();
-      }
-    } else {
-      buffer.push_back(c);
-    }
-  }
+  const size_t buffer_size = 1000000;
+  std::vector<char> buffer(buffer_size);
 
-  if (!buffer.empty()) {
-    callback(buffer);
+  std::string word;
+  do {
+    input.read(buffer.data(), buffer.size());
+    size_t chunk_size = input.gcount();
+    for (size_t i = 0; i < chunk_size; ++i) {
+      char c = NormalizeChar(buffer[i]);  // TODO: use lookup table to normalize chars
+      if (c == normalized::kSpace) {
+        if (!word.empty()) {
+          callback(word);
+          word.clear();
+        }
+      } else {
+        word.push_back(c);
+      }
+    }
+  } while(input.good());
+
+  if (!word.empty()) {
+    callback(word);
   }
 }
